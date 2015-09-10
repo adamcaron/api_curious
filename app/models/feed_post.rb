733270aa6cce696bfc3a5671763c6b2b3f9b407d
Adam Caron <adamwcaron@gmail.com>
2015-09-10 13:38:05 -0600

    Cleanly format variable assignments

--- app/models/feed_post.rb
+++ app/models/feed_post.rb
@@ -23,13 +23,15 @@ class FeedPost
     @id                = post_data[:id]
     @owner_profile_pic = post_data[:user][:profile_picture]
     @username          = post_data[:user][:username]
-    @location          = post_data[:location][:name] unless post_data[:location].nil?
+    @location          = post_data[:location][:name] unless
+                         post_data[:location].nil?
     @image_url         = post_data[:images][:standard_resolution][:url]
     @likes             = post_data[:likes][:count]
     @caption           = {
-                          username: post_data[:caption][:from][:username],
-                          text:     post_data[:caption][:text]
-                        } unless post_data[:caption].nil?
+               username: post_data[:caption][:from][:username],
+                   text: post_data[:caption][:text]
+                         } unless
+                         post_data[:caption].nil?
     @comments          = post_data[:comments][:data].map { |comment| PostComment.new(comment) }
   end
 end
