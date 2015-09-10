031bb4c77658ad00e63c10d4389712b167ce4254
Adam Caron <adamwcaron@gmail.com>
2015-09-09 19:21:57 -0600

    Display dynamic post images

--- app/models/feed_post.rb
+++ app/models/feed_post.rb
@@ -3,14 +3,17 @@ class FeedPost < OpenStruct
     @service ||= InstagramService.new
   end
 
-  def self.all(current_user)
-    service.feed(current_user.token)
+  def self.all(user)
+    response = service.feed(user.token)
+    response["data"].map { |post| FeedPost.new(post) }
   end
 
+  attr_reader :image_url
+
+  def initialize(post_info)
+    @image_url = post_info["images"]["standard_resolution"]["url"]
+  end
 end
 
-# feed.posts
-# feed.post
-
-# need to know current_user
-# create a new FeedPost
+# Caption Username ___________
+# r["data"].first["caption"]["from"]["username"]
