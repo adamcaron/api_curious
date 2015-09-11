630eb8ad8baaa7be398ccb4731892ba25a240d89
Adam Caron <adamwcaron@gmail.com>
2015-09-11 13:56:27 -0600

    Add test for displaying profile info on profile

--- spec/features/user_sees_profile_spec.rb
+++ spec/features/user_sees_profile_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.feature "User sees profile", type: :feature do
+
+  let(:user) { $user = create_user! }
+
   scenario "navigates to profile from homepage link" do
     VCR.use_cassette("user_sees_profile_test#navigates_to_profile_from_homepage_link") do
       visit root_path
@@ -13,5 +16,17 @@ RSpec.feature "User sees profile", type: :feature do
     end
   end
 
-  # "profile page has profile info"
+  scenario "profile displays user-specific profile info" do
+    VCR.use_cassette("user_sees_profile_test#profile_displays_user_specific_profile_info") do
+      visit root_path
+      login_user!
+      click_link "Log In"
+
+      click_link "adamcaron_"
+
+      expect(page).to have_content("Adam Caron")
+      expect(page).to have_content("adamcaron_")
+      expect(page).to have_content("NH🍁 -> CO🌲")
+    end
+  end
 end
